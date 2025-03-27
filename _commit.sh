c6acb0152be8917b12129b0a35b9e3347a4a0557
Roj <ez@roj.im>
2025-03-28 01:19:46 +0300

    Fix _commit.sh

--- _commit.sh
+++ _commit.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 git add -A
-git -c "user.name=github-actions[bot]" -c "user.email=41898282+github-actions[bot]@users.noreply.github.com" commit -m "Update results" || exit 0
+git config user.name "github-actions[bot]"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git commit -m "Update results" || exit 0
 until git pull --rebase && git push; do
     sleep 1
 done
