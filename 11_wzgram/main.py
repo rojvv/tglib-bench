3ad0b5e25bad9bdafce1d7291a817e4323dd814a
Riajul <93116400+rjriajul@users.noreply.github.com>
2026-07-09 22:24:51 +0600

    Optimize wzgram (#16)

--- 11_wzgram/main.py
+++ 11_wzgram/main.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import time
+import uvloop
 
 from pyrogram import Client
 from pyrogram import __version__
@@ -18,43 +19,39 @@ app = Client(
 
 
 async def main():
-    await app.start()
+    async with app:
+        chat_id, message_id = parse_message_link(env.MESSAGE_LINK)
 
-    chat_id, message_id = parse_message_link(env.MESSAGE_LINK)
+        timestamps: list[float] = []
+        message = await app.get_messages(chat_id=chat_id, message_ids=message_id)
+        if not message:
+            print("Message not found.", file=sys.stderr)
+            sys.exit(1)
 
-    timestamps: list[float] = []
-    message = await app.get_messages(chat_id=chat_id, message_ids=message_id)
-    if not message:
-        print("Message not found.", file=sys.stderr)
-        await app.stop()
-        sys.exit(1)
+        if not message.document:
+            print("Invalid message.", file=sys.stderr)
+            sys.exit(1)
 
-    if not message.document:
-        print("Invalid message.", file=sys.stderr)
-        await app.stop()
-        sys.exit(1)
+        file_size = message.document.file_size
 
-    file_size = message.document.file_size
+        timestamps.append(time.monotonic())
+        file_name = await message.download()
+        print(f"Downloaded {file_name}", file=sys.stderr)
+        timestamps.append(time.monotonic())
 
-    timestamps.append(time.time())
-    file_name = await message.download()
-    print(f"Downloaded {file_name}", file=sys.stderr)
-    timestamps.append(time.time())
+        timestamps.append(time.monotonic())
+        print("\nUploading...")
+        await app.send_document(
+            chat_id=env.CHAT_ID,
+            document=file_name,
+            force_document=True,
+        )
+        timestamps.append(time.monotonic())
 
-    timestamps.append(time.time())
-    print("\nUploading...")
-    await app.send_document(
-        chat_id=env.CHAT_ID,
-        document=file_name,
-        force_document=True,
-    )
-    timestamps.append(time.time())
-
-    with open("results.json", "w+") as f:
-        json.dump([file_size, timestamps, __version__], f)
-
-    await app.stop()
+        with open("results.json", "w+") as f:
+            json.dump([file_size, timestamps, __version__], f)
 
 
 if __name__ == "__main__":
+    uvloop.install()
     app.run(main())
